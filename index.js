d4f925a61be728ad8eeb1b4343fde391ada795ed
ssdh233 <songsongdahu233@gmail.com>
2022-05-19 22:53:01 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from "react";
+import ReactDOM from "react-dom";
+
+function App({ }) {
+  const [haifuData, setHaifuData] = useState(null);
+
+  const handleFileChange = useCallback((ev) => {
+    console.log("ev", ev.target.files);
+    const reader = new FileReader();
+    reader.readAsText(ev.target.files[0])
+
+    reader.onload = (ev) => {
+      setHaifuData(JSON.parse(ev.target.result))
+    }
+  }, [])
+
+  console.log(haifuData)
+
+  return <>
+    <label htmlFor="haifu">雀魂の牌譜を選択してください</label>
+    <div>
+      <input type="file" id="haifu" accept=".json" onChange={handleFileChange}></input>
+    </div>
+    <div>
+      {haifuData?.log.map((x, i) => {
+        if (x[16][2]) {
+          x[16][2] = x[16][2].slice(0, 4);
+          x[16][2][3] = x[16][2][3].replace("Haneman ", "跳満").replace("Mangan ", "満貫")
+        }
+        console.log(x[16][2]);
+        const url = `https://tenhou.net/6/#json=${encodeURIComponent(JSON.stringify({
+          title: ["", ""],
+          "name": ["Aさん", "Bさん", "Cさん", "Dさん"],
+          "rule": { "disp": "鳳南喰赤", "aka": 1 },
+          "log": [x]
+        }))}`;
+        return <div key={i}>
+          <a href={url}>{x[0][0] < 4 ? "東" : "南"}{(x[0][0] + 1) % 4}局 {x[0][1]}本場</a>
+          <input value={url} onFocus={event => { event.target.select(); }}></input>
+        </div>
+      })}
+    </div>
+  </>
+}
+
+ReactDOM.render(<App />, document.getElementById("react"));
